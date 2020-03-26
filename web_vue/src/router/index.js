384cacf3e402cb333f04dc7f8bdf0a86985f0164
BiBoBi250 <1605818994@qq.com>
2020-03-26 22:45:31 +0800

    add function

--- web_vue/src/router/index.js
+++ web_vue/src/router/index.js
@@ -1,26 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import App_Head from '@/components/App_Head'
-import App_Body from '@/components/App_Body'
+import Login from '@/components/login/Login'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/',
-      name: 'App_Head',
-      component: App_Head
-    },
-    {
-      path: '/',
-      name: 'App_Body',
-      component: App_Body
+      name: 'Login',
+      component: Login
     }
   ]
 
